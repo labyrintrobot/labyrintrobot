75b3fb89a67ad012362e68232dfe673043fd2c76
emibe <emibe709@student.liu.se>
2013-11-11 15:23:33 +0100

    Added Atmel studio projects

---
+++ libcommon/libcommon/libcommon.c
@@ -0,0 +1,15 @@
+/*
+ * libcommon.c
+ *
+ * Created: 11/11/2013 3:22:28 PM
+ *  Author: emibe709
+ */ 
+
+#include <avr/io.h>
+
+int function(void)
+{
+    //TODO:: Please write your application code
+
+    return 0;
+}

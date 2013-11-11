59a292251218636275789e2f818b4a8ec8a68161
vnowen <viktoria.nowen@gmail.com>
2013-11-11 18:26:32 +0100

    Försökte börja lite
    Fixa in- & ut

--- styrmodul/styrmodul/src/main.c
+++ styrmodul/styrmodul/src/main.c
@@ -25,10 +25,24 @@
  * Atmel Software Framework (ASF).
  */
 #include <asf.h>
+#include <avr/io.h>
 
 int main (void)
 {
 	board_init();
-
+	while (1)
+	{
+		PORTB = 0xff;
+	}
 	// Insert application code here, after the board has been initialized.
+}
+
+void board_init()
+{
+	DDRA = (1<<DDA3)|(1<<DDA2)|(1<<DDA1)|(1<<DDA0);
+	DDRB = (1<<DDB1)|(1<<DDB0);
+	DDRC = (1<<DDC1)|(1<<DDC0);
+	DDRD = ((1<<DDD6)|(1<<DDD5)|(1<<DDD4));
+	
+	
 }

34e670b4879a5eef9e42027cc5df18b1d65774ab
vnowen <viktoria.nowen@gmail.com>
2013-11-12 18:11:04 +0100

    C programming successful

--- styrmodul/styrmodul/src/main.c
+++ styrmodul/styrmodul/src/main.c
@@ -26,23 +26,20 @@
  */
 #include <asf.h>
 #include <avr/io.h>
+#include <inttypes.h>
+#include <stdio.h>
 
 int main (void)
 {
 	board_init();
-	while (1)
+	
+	uint8_t temp;
+	
+	for(;;)
 	{
-		PORTB = 0xff;
+		temp = PINA;	
+		PORTB = temp;
 	}
 	// Insert application code here, after the board has been initialized.
 }
 
-void board_init()
-{
-	DDRA = (1<<DDA3)|(1<<DDA2)|(1<<DDA1)|(1<<DDA0);
-	DDRB = (1<<DDB1)|(1<<DDB0);
-	DDRC = (1<<DDC1)|(1<<DDC0);
-	DDRD = ((1<<DDD6)|(1<<DDD5)|(1<<DDD4));
-	
-	
-}

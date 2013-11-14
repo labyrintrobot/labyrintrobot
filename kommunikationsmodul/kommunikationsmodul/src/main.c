066571da05b64eb662165dac9980870afc00a62d
kribo852 <kribo852@student.liu.se>
2013-11-14 14:48:40 +0100

    fungerande avbrott

--- kommunikationsmodul/kommunikationsmodul/src/main.c
+++ kommunikationsmodul/kommunikationsmodul/src/main.c
@@ -20,7 +20,7 @@ int communicationid=1;
 int steerid=2;
 int sensorid=3;
 
-volatile int testcounter=0;
+volatile int testcounter=0b01010101;
 
 //interrupt routine for setting the interrupt vector note that the StatusRegister
 //must be saved
@@ -28,13 +28,13 @@ ISR(INT0_vect){
 	char cSREG;
 	cSREG = SREG;/* store SREG value */
 	/* disable interrupts during timed sequence */
-	cli();
+	//cli();
 	
 	testcounter++;
 	
 	SREG = cSREG; /* restore SREG value (I-bit) */
 	
-	sei(); 
+	//sei(); 
 }
 
 //function that recives data via I2C if interupt is raised
@@ -61,6 +61,20 @@ int main (void)
 	int interuptvector=0;
 	
 	DDRB=0xFF;
+	
+	PORTB=0;
+	
+	sei();//enable interrupts
+	
+	EIMSK&=0b11111110;// clear interrupt request mask
+	EICRA|=0b00000011;// interrupts on rising edge
+	
+	EICRA&=0b11111110;// interrupts on falling edge
+	
+	EIFR|=0b00000001; 
+	EIMSK|=0b00000001;// set interrupt request mask
+	
+	//SREG=;
 	
 	//main loop 
 	while(1){

e9bfc6685d079ccf6a39589d0b7ec1bce21cace8
emibe <emibe709@student.liu.se>
2013-11-28 14:41:14 +0100

    Moved queue files

---
+++ kommunikationsmodul/kommunikationsmodul/src/queue.c
@@ -0,0 +1,51 @@
+/*
+ * queue.c
+ *
+ *  Author: Emil Berg, Kristoffer Borg
+ */ 
+
+#include <inttypes.h>
+#include <stdlib.h>
+#include "queue.h"
+
+void queue_init(queue* q) {
+	q->first = 0;
+	q->last = 0;
+}
+
+void queue_add(queue* q, uint8_t header, uint8_t data) {
+	queue_element* e = malloc(sizeof(queue_element));
+	e->data = data;
+	e->header = header;
+	e->next = 0;
+	
+	if (q->last == 0) {
+		q->first = e;
+		q->last = e;
+	} else {
+		q->last->next = e;
+	}
+	
+	q->last = e;
+}
+
+void queue_remove(queue* q, uint8_t* header, uint8_t* data) {
+	
+	queue_element* e = q->first;
+	
+	if (e == 0) {
+		*header = 0;
+		*data = 0;
+	} else {
+		q->first = q->first->next;
+		
+		*header = e->header;
+		*data = e->data;
+		
+		free(e);
+	}
+}
+
+bool queue_is_empty(queue* q) {
+	return (q->first == 0);
+}

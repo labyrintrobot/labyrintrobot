87eba09c6740da0439376615ea67cd87499ddf28
emibe <emibe709@student.liu.se>
2013-12-02 09:57:26 +0100

    GUI should now function when connection lost. No start button now.

--- GUI/src/application/BluetoothAdapter.java
+++ GUI/src/application/BluetoothAdapter.java
@@ -1,45 +1,73 @@
 package application;
 
+import java.io.DataOutputStream;
 import java.io.IOException;
+import java.io.InputStream;
+import java.util.Arrays;
 import java.util.Random;
 
+import javax.microedition.io.Connector;
+import javax.microedition.io.StreamConnection;
 import javafx.application.Platform;
 
 public class BluetoothAdapter {
-	
+
 	private static final boolean DEBUG = false;
-	
-	private boolean running = true;
-	
+
+	private final String bluetoothUrl;
 	private final IMessageReceiver receiver;
-	
 	private final Random rng = new Random();
-	
-	private final BluetoothCommunicator bluetoothCommunicator;
-	
-	public BluetoothAdapter(IMessageReceiver receiver) {
+
+	private boolean isSetup = false;
+	private boolean running = true;
+	private StreamConnection conn;
+	private DataOutputStream os;
+	private InputStream is;
+
+	public BluetoothAdapter(String bluetoothUrl, IMessageReceiver receiver) {
+		this.bluetoothUrl = bluetoothUrl;
 		this.receiver = receiver;
-		this.bluetoothCommunicator = new BluetoothCommunicator();
 	}
-	
-	public void setup(String bluetoothUrl) throws IOException {
+
+	private void setup() throws IOException {
 		if (! DEBUG) {
-			this.bluetoothCommunicator.setup(bluetoothUrl);
+			conn = (StreamConnection) Connector.open(this.bluetoothUrl);
+			os = new DataOutputStream(conn.openOutputStream());
+			is = conn.openDataInputStream();
 		}
+		isSetup = true;
+		receiver.communicationGained();
 	}
-	
+
 	public interface IMessageReceiver {
 		public void receiveMessage(int header, int data);
+		public void communicationGained();
+		public void communicationLost();
 	}
-	
-	public void sendMessage(int header, int data) throws IOException {
-		int[] b = new int[2];
-		b[0] = header;
-		b[1] = data;
-		this.bluetoothCommunicator.sendMessageToDevice(b);
+
+	public void sendMessage(int header, int data) {
+		if (! DEBUG) {
+			int[] b = new int[2];
+			b[0] = header;
+			b[1] = data;
+			if (isSetup) {
+				try {
+					sendMessageToDevice(b);
+				} catch (IOException e) {
+					this.teardown();
+					Platform.runLater(new Runnable() {
+
+						@Override
+						public void run() {
+							receiver.communicationLost();
+						}
+					});
+				}
+			}
+		}
 	}
-	
-	public void receiveMessages() throws IOException {
+
+	public void receiveMessages() {
 		while (running) {
 			if (DEBUG) {
 				try {
@@ -47,7 +75,7 @@ public class BluetoothAdapter {
 				} catch (InterruptedException e) {
 					// Do nothing
 				}
-				
+
 				final byte header = (byte)rng.nextInt(0x0C);
 				final int data;
 				if (header == 0x01) {
@@ -55,30 +83,86 @@ public class BluetoothAdapter {
 				} else {
 					data = rng.nextInt(0xFF);
 				}
-				
+
 				Platform.runLater(new Runnable() {
-					
+
 					@Override
 					public void run() {
 						receiver.receiveMessage(header, data);
 					}
 				});
 			} else {
-				final int[] ret = this.bluetoothCommunicator.receiveMessage(2);
-				
-				Platform.runLater(new Runnable() {
-					
-					@Override
-					public void run() {
-						receiver.receiveMessage(ret[0], ret[1]);
+				if (isSetup) {
+					try {
+						final int[] ret = receiveMessage(2);
+
+						Platform.runLater(new Runnable() {
+
+							@Override
+							public void run() {
+								receiver.receiveMessage(ret[0], ret[1]);
+							}
+						});
+					} catch (IOException e) {
+						this.teardown();
+						Platform.runLater(new Runnable() {
+
+							@Override
+							public void run() {
+								receiver.communicationLost();
+							}
+						});
 					}
-				});
-				this.bluetoothCommunicator.teardown();
+				} else {
+					try {
+						this.setup();
+					} catch (IOException e) {
+						this.teardown();
+					}
+				}
+
 			}
 		}
+		this.teardown();
 	}
-	
+
 	public void exit() {
 		running = false;
+	}
+
+	private void teardown() {
+		if (isSetup) {
+			try {
+				os.close();
+				is.close();
+				conn.close();
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+		this.isSetup = false;
+	}
+
+	private synchronized int[] receiveMessage(int length) throws IOException {
+
+		int[] ret = new int[length];
+		for (int i = 0; i < length; i++) {
+			ret[i] = is.read();
+			if (ret[i] == -1) {
+				throw new IOException();
+			}
+		}
+		System.out.println("Received: " + Arrays.toString(ret));
+
+		return ret;
+	}
+
+	private synchronized void sendMessageToDevice(int[] message) throws IOException {
+		System.out.println("Sent: " + Arrays.toString(message));
+		for (int i = 0; i < message.length; i++) {
+			os.write(message[i]);
+		}
+		os.flush();
 	}
 }
--- GUI/src/application/BluetoothStage.java
+++ GUI/src/application/BluetoothStage.java
@@ -1,87 +1,531 @@
 package application;
 
 import java.io.IOException;
-
-import javax.bluetooth.BluetoothStateException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Set;
 
 import javafx.application.Application;
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.geometry.Pos;
+import javafx.scene.Node;
 import javafx.scene.Scene;
+import javafx.scene.chart.LineChart;
+import javafx.scene.chart.NumberAxis;
+import javafx.scene.chart.XYChart;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.control.ProgressIndicator;
+import javafx.scene.control.Slider;
+import javafx.scene.control.TextArea;
+import javafx.scene.input.KeyEvent;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
+import javafx.stage.WindowEvent;
+import application.ChartSelectorPad.SelectedToggleButton;
 
-public class BluetoothStage extends Application {
+public class BluetoothStage extends Application implements BluetoothAdapter.IMessageReceiver {
 	
-	//private String bluetoothUrl;
-	//private final BluetoothDiscoveryListener bluetoothDiscoveryListener;
-	private final Button startButton;
+	long startTime = System.currentTimeMillis();
+	
+	private final String BLUETOOTH_URL = "btspp://00066603A696:1;authenticate=true;encrypt=false;master=false";
+	private static final String PAUSE_TEXT = "Pause";
+	private static final String RESUME_TEXT = "Resume";
+	private static final int CHART_BATCHES_MAX = 100;
+
+	private final ControllerAdapter controllerAdapter;
+	private final BluetoothAdapter bluetoothAdapter;
+	private final ControlPad controlPad;
+	private final ChartSelectorPad chartSelectorPad;
+	
+	private final LineChart<Number, Number> lineChart;
+	private final Slider minSlider;
+	private final Slider maxSlider;
+	private final Label minSliderLabel;
+	private final Label maxSliderLabel;
 	private final ProgressIndicator progressIndicator;
 
-	public BluetoothStage() {
-		/*bluetoothDiscoveryListener = new BluetoothDiscoveryListener("FireFly-A696", new IServicesDiscoveredCallback() {
+	private final TextArea errorLog;
 
-			@Override
-			public void servicesDiscovered(String bluetoothUrl) {
-				BluetoothStage.this.bluetoothUrl = bluetoothUrl;
-				startButton.setDisable(false);
-				Platform.runLater(new Runnable() {
-					
-					@Override
-					public void run() {
-						progressIndicator.setProgress(2.0);
-					}
-				});
-			}
-		});
-		bluetoothDiscoveryListener.findDevices();*/
-		startButton = new Button("Start");
-		progressIndicator = new ProgressIndicator();
+	private boolean paused = false;
+	
+	private int chartBatches = 0;
+	
+	private static class TimeValuePair {
+		public final long time;
+		public final int value;
+		
+		public TimeValuePair(long time, int value) {
+			this.time = time;
+			this.value = value;
+		}
 	}
 	
-	@Override
-	public void start(final Stage primaryStage) {
+	private final List<TimeValuePair> distanceLeftShortList = new ArrayList<>();
+	private final List<TimeValuePair> distanceLeftLongList = new ArrayList<>();
+	private final List<TimeValuePair> distanceForwardLeftList = new ArrayList<>();
+	private final List<TimeValuePair> distanceForwardCenterList = new ArrayList<>();
+	private final List<TimeValuePair> distanceForwardRightList = new ArrayList<>();
+	private final List<TimeValuePair> distanceRightLongList = new ArrayList<>();
+	private final List<TimeValuePair> distanceRightShortList = new ArrayList<>();
+	private final List<TimeValuePair> tapeList = new ArrayList<>();
+	private final List<TimeValuePair> controlErrorList = new ArrayList<>();
+	
+	public static void main(String[] args) {
+		launch(args);
+	}
+	
+	public BluetoothStage() {
 
+		this.bluetoothAdapter = new BluetoothAdapter(BLUETOOTH_URL, this);
+
+		this.lineChart = generateRealTimeChart("Forward left, short", "y", 256);
+		
+		this.minSlider = new Slider(0.0, 1.0, 0.0);
+		this.maxSlider = new Slider(0.0, 1.0, 1.0);
+		this.minSliderLabel = new Label("Min");
+		this.maxSliderLabel = new Label("Max");
+		
+		this.progressIndicator = new ProgressIndicator(-1.0);
+		
+		this.controllerAdapter = new ControllerAdapter(bluetoothAdapter);
+		this.controlPad = new ControlPad();
+		this.chartSelectorPad = new ChartSelectorPad(new ChartSelectorPad.ToggleCallback() {
+			
+			@Override
+			public void callback(ChartSelectorPad.SelectedToggleButton stb) {
+		        GetFromSelRetPair gfsrp = getFromSel(stb);
+				setLineChartData(gfsrp.l, gfsrp.s);
+			}
+		});
+		
+		this.errorLog = new TextArea();
+	}
+	
+	private LineChart<Number, Number> generateRealTimeChart(String name, String yAxisText, int maxY) {
+		
+		// Setup
+		NumberAxis xAxis = new NumberAxis(0, 100, 10);
+        NumberAxis yAxis = new NumberAxis(0, maxY, maxY/8);
+        xAxis.setLabel("Time");
+        yAxis.setLabel(yAxisText);
+        xAxis.setForceZeroInRange(false);
+        
+        LineChart<Number, Number> lc = new LineChart<Number, Number>(xAxis, yAxis);
+        lc.setAnimated(false);
+        lc.setCreateSymbols(false);
+        
+        lc.setTitle(name);
+        lc.setLegendVisible(false);
+        
+        Set<Node> lookupAll = lc.lookupAll(".series0");
+        for (Node n : lookupAll) {
+        	n.setStyle("-fx-stroke-width: 1px;-fx-stroke: black;");
+        }
+        
+        return lc;
+	}
+	
+	private void setLineChartData(List<TimeValuePair> data, String title) {
+        XYChart.Series<Number, Number> series = new XYChart.Series<Number, Number>();
+        
+        lineChart.setTitle(title);
+        
+        int lower = (int) (minSlider.getValue() * data.size());
+        int upper = (int) (maxSlider.getValue() * data.size());
+		
+        for (int i = lower; i < upper; i++) {
+        	TimeValuePair tvp = data.get(i);
+        	series.getData().add(new XYChart.Data<Number, Number>(tvp.time, tvp.value));
+        }
+        
+        NumberAxis xAxis = (NumberAxis) lineChart.getXAxis();
+        
+        if (data.size() != 0) {
+        	xAxis.setLowerBound(data.get(lower).time);
+        	xAxis.setUpperBound(data.get(upper - 1).time);
+        }
+        
+        lineChart.getData().remove(0, lineChart.getData().size());
+        lineChart.getData().add(series);
+        
+        Set<Node> lookupAll = lineChart.lookupAll(".series0");
+        for (Node n : lookupAll) {
+        	n.setStyle("-fx-stroke-width: 1px;-fx-stroke: black;");
+        }
+	}
+	
+	public void start(Stage primaryStage) {
+		
+		minSlider.valueProperty().addListener(new ChangeListener<Number>() {
+
+			@Override
+			public void changed(ObservableValue<? extends Number> arg0,
+					Number arg1, Number arg2) {
+				
+				minSlider.setValue(Math.min(minSlider.getValue(), maxSlider.getValue()));
+				
+				if (paused) {
+			        GetFromSelRetPair gfsrp = getFromSel(chartSelectorPad.getSelected());
+					setLineChartData(gfsrp.l, gfsrp.s);
+				}
+			}
+		});
+		
+		maxSlider.valueProperty().addListener(new ChangeListener<Number>() {
+
+			@Override
+			public void changed(ObservableValue<? extends Number> arg0,
+					Number arg1, Number arg2) {
+				
+				maxSlider.setValue(Math.max(minSlider.getValue(), maxSlider.getValue()));
+				
+				if (paused) {
+			        GetFromSelRetPair gfsrp = getFromSel(chartSelectorPad.getSelected());
+					setLineChartData(gfsrp.l, gfsrp.s);
+				}
+			}
+		});
+
+		controlPad.pressStop();
+		errorLog.setEditable(false);
+		
 		try {
+			
 			primaryStage.setTitle("Labyrintrobot");
+			final Button pauseButton = new Button();
+			if (paused) {
+				pauseButton.setText(RESUME_TEXT);
+			} else {
+				pauseButton.setText(PAUSE_TEXT);
+			}
+
+			VBox northBox = new VBox();
+			HBox southBox = new HBox();
+			VBox centerBox = new VBox();
+
+			northBox.setAlignment(Pos.TOP_CENTER);
+			southBox.setAlignment(Pos.BOTTOM_CENTER);
+			centerBox.setAlignment(Pos.CENTER);
+			
+			northBox.getChildren().addAll(minSlider, minSliderLabel, maxSliderLabel, maxSlider);
+			centerBox.getChildren().addAll(progressIndicator, pauseButton, lineChart);
+			southBox.getChildren().addAll(errorLog, controlPad, chartSelectorPad);
 
 			BorderPane root = new BorderPane();
-			HBox southBox = new HBox();
-			southBox.getChildren().addAll(startButton/*, progressIndicator*/);
+			root.setTop(northBox);
 			root.setBottom(southBox);
-			southBox.setAlignment(Pos.BOTTOM_CENTER);
-			
-			Scene scene = new Scene(root, 320, 120);
+			root.setCenter(centerBox);
+			Scene scene = new Scene(root, 1280, 1024);
 			primaryStage.setScene(scene);
-			primaryStage.show();
-			
-			//startButton.setDisable(true);
-			
-			startButton.setOnAction(new EventHandler<ActionEvent>() {
-				@Override public void handle(ActionEvent e) {
-					//Stage stage = new PresentationStage(bluetoothUrl);
-					Stage stage;
+
+			EventHandler<KeyEvent> pressEvent = new EventHandler<KeyEvent>() {
+
+				@Override
+				public void handle(KeyEvent t) {
 					try {
-						stage = new PresentationStage("btspp://00066603A696:1;authenticate=true;encrypt=false;master=false");
-						stage.show();
-						primaryStage.hide();
-					} catch (IOException e1) {
-						System.err.println("Unable to open Bluetooth connection");
-						//e1.printStackTrace();
+						switch (t.getCode()) {
+						case RIGHT:
+							controllerAdapter.pressRight();
+							break;
+						case UP:
+							controllerAdapter.pressUp();
+							break;
+						case LEFT:
+							controllerAdapter.pressLeft();
+							break;
+						case DOWN:
+							controllerAdapter.pressDown();
+							break;
+						case C:
+							controllerAdapter.pressC();
+							break;
+						case Q:
+							controllerAdapter.pressQ();
+							break;
+						case W:
+							controllerAdapter.pressW();
+							break;
+						default:
+							// Do nothing
+							break;
+						}
+					} catch (IOException e) {
+						e.printStackTrace();
+					}
+				}
+			};
+
+			EventHandler<KeyEvent> releaseEvent = new EventHandler<KeyEvent>() {
+
+				@Override
+				public void handle(KeyEvent t) {
+					try {
+						switch (t.getCode()) {
+						case RIGHT:
+							controllerAdapter.releaseRight();
+							break;
+						case UP:
+							controllerAdapter.releaseUp();
+							break;
+						case LEFT:
+							controllerAdapter.releaseLeft();
+							break;
+						case DOWN:
+							controllerAdapter.releaseDown();
+							break;
+						case C:
+							controllerAdapter.releaseC();
+							break;
+						case Q:
+							controllerAdapter.releaseQ();
+							break;
+						case W:
+							controllerAdapter.releaseW();
+							break;
+						default:
+							// Do nothing
+							break;
+						}
+					} catch (IOException e) {
+						e.printStackTrace();
+					}
+				}
+			};
+
+			pauseButton.setOnAction(new EventHandler<ActionEvent>() {
+				@Override public void handle(ActionEvent e) {
+					paused = !paused;
+					if (paused) {
+						pauseButton.setText(RESUME_TEXT);
+					} else {
+						pauseButton.setText(PAUSE_TEXT);
+						maxSlider.setValue(1.0);
 					}
 				}
 			});
+
+			primaryStage.setOnCloseRequest(new EventHandler<WindowEvent>() {
+
+				@Override
+				public void handle(WindowEvent we) {
+					bluetoothAdapter.exit();
+				}
+			});
+
+			pauseButton.setFocusTraversable(false);
+
+			primaryStage.addEventFilter(KeyEvent.KEY_PRESSED, pressEvent);
+			primaryStage.addEventFilter(KeyEvent.KEY_RELEASED, releaseEvent);
+
+			primaryStage.show();
+
+			listen();
 
 		} catch(Exception e) {
 			e.printStackTrace();
 		}
 	}
+	
+	private static class GetFromSelRetPair {
+		public String s;
+		public List<TimeValuePair> l;
+	}
+	
+	private GetFromSelRetPair getFromSel(ChartSelectorPad.SelectedToggleButton stb) {
+		
+		GetFromSelRetPair gfsrp = new GetFromSelRetPair();
+		
+		switch (stb) {
+		case DISTANCE_LEFT_SHORT:
+			gfsrp.s = "Distance left, short";
+			gfsrp.l = distanceLeftShortList;
+			break;
+		case DISTANCE_LEFT_LONG:
+			gfsrp.s = "Distance left, long";
+			gfsrp.l = distanceLeftShortList;
+			break;
+		case DISTANCE_FORWARD_CENTER:
+			gfsrp.s = "Distance forward, center";
+			gfsrp.l = distanceLeftShortList;
+			break;
+		case DISTANCE_FORWARD_LEFT:
+			gfsrp.s = "Distance forward, left";
+			gfsrp.l = distanceLeftShortList;
+			break;
+		case DISTANCE_FORWARD_RIGHT:
+			gfsrp.s = "Distance forward, right";
+			gfsrp.l = distanceLeftShortList;
+			break;
+		case DISTANCE_RIGHT_LONG:
+			gfsrp.s = "Distance right, long";
+			gfsrp.l = distanceLeftShortList;
+			break;
+		case DISTANCE_RIGHT_SHORT:
+			gfsrp.s = "Distance right, short";
+			gfsrp.l = distanceLeftShortList;
+			break;
+		case TAPE:
+			gfsrp.s = "Tape width";
+			gfsrp.l = distanceLeftShortList;
+			break;
+		case CONTROL_ERROR:
+			gfsrp.s = "Control error";
+			gfsrp.l = distanceLeftShortList;
+			break;
+		default:
+			// Should not happen
+			return null;
+		}
+		
+		return gfsrp;
+	}
 
-	public static void main(String[] args) {
-		launch(args);
+	private void listen() {
+		Thread listenerThread = new Thread(new Runnable() {
+
+			@Override
+			public void run() {
+				bluetoothAdapter.receiveMessages();
+			}
+
+		});
+		listenerThread.setDaemon(true);
+
+		listenerThread.start();
+	}
+
+	private void log(TextArea ta, String text) {
+		ta.setText(ta.getText() + text + '\n');
+		ta.positionCaret(ta.getText().length()); // Scroll to end
+	}
+	
+	private void updateLineChartData(List<TimeValuePair> l, int newData, SelectedToggleButton stb, String text) {
+		l.add(new TimeValuePair((System.currentTimeMillis() - startTime) / 100, newData));
+		if (!paused && chartSelectorPad.getSelected() == stb && chartBatches >= CHART_BATCHES_MAX) {
+			chartBatches = 0;
+			setLineChartData(l, text);
+		}
+	}
+
+	@Override
+	public void receiveMessage(int header, int data) {
+		chartBatches++;
+		switch (header) {
+
+		case 0x00:
+			log(errorLog, "Unexpected header " + Integer.toHexString(header) + ": 0x" + Integer.toHexString(data));
+			break;
+		
+		case 0x01:
+			switch (data) {
+
+			case 0x00:
+				controlPad.pressForward();
+				break;
+
+			case 0x01:
+				controlPad.pressBackwards();
+				break;
+
+			case 0x02:
+				controlPad.pressRightForward();
+				break;
+
+			case 0x03:
+				controlPad.pressLeftForward();
+				break;
+
+			case 0x04:
+				controlPad.pressRotateRight();
+				break;
+
+			case 0x05:
+				controlPad.pressRotateLeft();
+				break;
+
+			case 0x06:
+				controlPad.pressStop();
+				break;
+
+			default:
+				log(errorLog, "Illegal data received for header 0x" + Integer.toHexString(header) + ": 0x" + Integer.toHexString(data));
+				break;
+			}
+			break;
+			
+		case 0x02:
+			log(errorLog, "Unexpected header " + Integer.toHexString(header) + ": 0x" + Integer.toHexString(data));
+			break;
+
+		case 0x03:
+			updateLineChartData(distanceLeftShortList, data, SelectedToggleButton.DISTANCE_LEFT_SHORT, "Distance left, short");
+			break;
+
+		case 0x04:
+			updateLineChartData(distanceLeftLongList, data, SelectedToggleButton.DISTANCE_LEFT_LONG, "Distance left, long");
+			break;
+
+		case 0x05:
+			updateLineChartData(distanceForwardLeftList, data, SelectedToggleButton.DISTANCE_FORWARD_LEFT, "Distance forward left");
+			break;
+
+		case 0x06:
+			updateLineChartData(distanceForwardCenterList, data, SelectedToggleButton.DISTANCE_FORWARD_CENTER, "Distance forward center");
+			break;
+
+		case 0x07:
+			updateLineChartData(distanceForwardRightList, data, SelectedToggleButton.DISTANCE_FORWARD_RIGHT, "Distance forward right");
+			break;
+
+		case 0x08:
+			updateLineChartData(distanceRightLongList, data, SelectedToggleButton.DISTANCE_RIGHT_LONG, "Distance right long");
+			break;
+
+		case 0x09:
+			updateLineChartData(distanceRightShortList, data, SelectedToggleButton.DISTANCE_RIGHT_SHORT, "Distance right short");
+			break;
+
+		case 0x0A:
+			updateLineChartData(controlErrorList, data, SelectedToggleButton.CONTROL_ERROR, "Control error");
+			break;
+
+		case 0x0B:
+			updateLineChartData(tapeList, data, SelectedToggleButton.TAPE, "Tape width");
+			break;
+			
+		case 0x0C:
+			log(errorLog, "Error code 0x" + Integer.toHexString(data));
+			break;
+			
+		case 0x0D:
+			if (data != 0x00) {
+				log(errorLog, "Invalid ping data: 0x" + Integer.toHexString(data));
+			}
+			// Just respond
+			bluetoothAdapter.sendMessage(header, 0x00);
+			break;
+
+		default:
+			log(errorLog, "Invalid header: 0x" + Integer.toHexString(header) + ", data: 0x" + Integer.toHexString(data));
+			break;
+		}
+	}
+
+	@Override
+	public void communicationGained() {
+		// Finished
+		this.progressIndicator.setProgress(1.0);
+	}
+
+	@Override
+	public void communicationLost() {
+		// Keep "working"
+		this.progressIndicator.setProgress(-1.0);
 	}
 }

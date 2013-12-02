6a82f43d03507b5a4266b9dc659f4970faa92306
emibe <emibe709@student.liu.se>
2013-12-02 18:28:35 +0100

    Ping fixes. Timer probably works.

--- kommunikationsmodul/kommunikationsmodul/src/communication.c
+++ kommunikationsmodul/kommunikationsmodul/src/communication.c
@@ -12,9 +12,13 @@
 #include "USART.h"
 #include "communication.h"
 
+void send_error(uint8_t error_code);
 void enable_irqs(void);
 void enable_timer(void);
 int TWI_master_init_slaves(void);
+
+// Define to enable ping
+//#define USE_PING
 
 // Global variables
 volatile bool control_module_interrupt = false;
@@ -25,28 +29,6 @@ volatile bool firefly_received_data = false;
 #ifdef USE_PING
 volatile bool ping_received = false;
 #endif
-
-// Interrupt init
-// Remember to set firefly interrupt flag
-void enable_irqs() {
-	
-	EICRA |= (1 << ISC00) | (1 << ISC01); // Interrupt irq0 on rising edge
-	EICRA |= (1 << ISC10) | (1 << ISC11); // Interrupt irq1 on rising edge
-	EIMSK |= (1 << INT0) | (1 << INT1); // Enable INT0 and INT1
-	EIFR |= (1 << INTF0) | (1 << INTF1);
-#ifdef USE_PING
-	TIMSK = (1<<TOIE1); // Enable timer 1
-#endif
-	
-	sei();
-}
-
-void enable_timer() {
-#ifdef USE_PING
-	TCNT1 = 0x00;
-	TCCR1B = (1<<CS02); // 256 prescaler. 14000000 / 65536 / 256 = 0.8 times per second.
-#endif
-}
 
 //Firefly interrupt routine
 ISR(USART0_RX_vect) {
@@ -84,13 +66,43 @@ ISR(INT1_vect) {
 ISR(TIMER0_OVF_vect) {
 	
 	if (! ping_received) {
-		// Send TWI
+		// No ping. Send stop to control module.
+		int twi_send_err = TWI_master_send_message(TWI_SENSOR_MODULE_ADDRESS, 0x00, 0x06);
+		if (twi_send_err) {
+			send_error(0x03);
+		}
 	}
 	// Send new ping
 	USART_transmit(0x0D, 0x00);
 	ping_received = false;
 }
 #endif
+
+void send_error(uint8_t error_code) {
+	USART_transmit(0x0C, error_code);
+}
+
+// Interrupt init
+// Remember to set firefly interrupt flag
+void enable_irqs() {
+	
+	EICRA |= (1 << ISC00) | (1 << ISC01); // Interrupt irq0 on rising edge
+	EICRA |= (1 << ISC10) | (1 << ISC11); // Interrupt irq1 on rising edge
+	EIMSK |= (1 << INT0) | (1 << INT1); // Enable INT0 and INT1
+	EIFR |= (1 << INTF0) | (1 << INTF1);
+	#ifdef USE_PING
+	TIMSK1 |= (1<<TOIE1); // Enable timer 1
+	#endif
+	
+	sei();
+}
+
+void enable_timer() {
+	#ifdef USE_PING
+	TCNT1 = 0x00;
+	TCCR1B = (1<<CS02); // 256 prescaler. 14000000 / 65536 / 256 = 0.8 times per second.
+	#endif
+}
 
 void mainfunction() {
 	
@@ -107,42 +119,50 @@ void mainfunction() {
 			
 			int twi_send_err = 0;
 			
-			//send to right instance depending on header
+			// Send to right instance depending on header
 			if (header == 0x00) {
-				// Firefly is sending control commands, relay to control module
+				// FireFly is sending control commands. Relay to control module.
 				cli();
 				twi_send_err = TWI_master_send_message(TWI_CONTROL_MODULE_ADDRESS, header, data);
 				sei();
 			} else if (header == 0x01) {
-				// Control module is sending, relay to firefly
+				// Control module is sending. Relay to firefly
+				cli();
 				USART_transmit(header, data);
+				sei();
 			} else if (header == 0x02) {
-				// Firefly is sending a calibration command, relay to sensor module
+				// FireFly is sending a calibration command. Relay to sensor module.
 				cli();
 				twi_send_err = TWI_master_send_message(TWI_SENSOR_MODULE_ADDRESS, header, data);
 				sei();
 			} else if (header >= 0x03 && header <= 0x0B) {
-				// Sensor module is sending, relay to styr module and firefly
+				// Sensor module is sending. Relay to control module and FireFly.
 				cli();
 				twi_send_err = TWI_master_send_message(TWI_CONTROL_MODULE_ADDRESS, header, data);
+				USART_transmit(header, data);
 				sei();
-				USART_transmit(header, data);
 			} else if (header == 0x0C) {
-				// Error. Relay to firefly
+				// Error. Relay to FireFly.
+				cli();
 				USART_transmit(header, data);
+				sei();
 			} else if (header == 0x0D) {
 #ifdef USE_PING
 				ping_received = true;
 #endif
 			} else {
 				// Invalid header. Send error message
-				USART_transmit(0x0C, 0x00);
+				cli();
+				send_error(0x00);
+				sei();
 			}
 			
 			// If there was any error with TWI
 			if (twi_send_err) {
 				// TWI error. Send error message.
-				USART_transmit(0x0C, 0x01);
+				cli();
+				send_error(0x01);
+				sei();
 			}
 			
 			received_data = false;
@@ -177,7 +197,9 @@ void mainfunction() {
 		}
 		if (twi_rec_err) {
 			// TWI error. Send error message.
-			USART_transmit(0x0C, 0x02);
+			cli();
+			send_error(0x02);
+			sei();
 		}
 	}
 }
